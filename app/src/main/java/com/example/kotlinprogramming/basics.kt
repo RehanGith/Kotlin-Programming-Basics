92eb27528929232967378564cecc5097c3eb7476
RehanGith <chrehan1007@gmail.com>
2024-06-22 06:09:55 +0700

    ArrayList and Array in For loop

--- app/src/main/java/com/example/kotlinprogramming/basics.kt
+++ app/src/main/java/com/example/kotlinprogramming/basics.kt
@@ -1,8 +1,22 @@
 package com.example.kotlinprogramming
 
 fun main() {
-    println("Hello, World!")
-    for(i in 1..50 step 2) {
-        println(i)
+//    println("Hello, World!")
+//    for(i in 1..50 step 2) {
+//        println(i)
+//    }
+//    var arr: Array<Int> = arrayOf(1,2,3,4,5)
+//    for(i in arr) {
+//        println(i)
+//    }
+    val arr:ArrayList<Int> = ArrayList()
+    for(i in 0..10) {
+        arr.add(i, 2*i)
+    }
+    arr?.let { nonNullArr ->
+        for(i in nonNullArr) {
+            println(i)
+        }
+
     }
 }
