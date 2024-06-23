3e2c2076f0ccab56cc0a7318fe3507767f214053
RehanGith <chrehan1007@gmail.com>
2024-06-23 11:10:23 +0700

    Erase recent work

--- app/src/main/java/com/example/kotlinprogramming/basics.kt
+++ app/src/main/java/com/example/kotlinprogramming/basics.kt
@@ -1,16 +1,6 @@
 package com.example.kotlinprogramming
 
 fun main() {
-    var nonNullable:String = "Muhammad"
-  //  nonNullable = null          GIVES AN ERROR MESSAGE BECAUSE WE CAN'T ASSIGN NULL TO NONNULLABLE
-    var nullable:String? = "Rehan"
-    //nullable = null
-    nonNullable = nullable ?: "Name"
-    println("NonNullable name is $nonNullable")
-}
 
-fun average(a: Double, b:Double): Double {
-    val ave:Double
-    ave = a+b/2
-    return ave
+
 }
