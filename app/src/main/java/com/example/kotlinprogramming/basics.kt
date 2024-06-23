f238e2ebfbcb65410bd207963ee5ca7ed92e99f9
RehanGith <chrehan1007@gmail.com>
2024-06-23 09:41:16 +0700

    using nullable and elvis operator

--- app/src/main/java/com/example/kotlinprogramming/basics.kt
+++ app/src/main/java/com/example/kotlinprogramming/basics.kt
@@ -1,11 +1,16 @@
 package com.example.kotlinprogramming
 
 fun main() {
-
+    var nonNullable:String = "Muhammad"
+  //  nonNullable = null          GIVES AN ERROR MESSAGE BECAUSE WE CAN'T ASSIGN NULL TO NONNULLABLE
+    var nullable:String? = "Rehan"
+    //nullable = null
+    nonNullable = nullable ?: "Name"
+    println("NonNullable name is $nonNullable")
 }
 
 fun average(a: Double, b:Double): Double {
-    var ave:Double
+    val ave:Double
     ave = a+b/2
     return ave
 }
